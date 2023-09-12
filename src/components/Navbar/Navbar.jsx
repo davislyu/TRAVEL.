2235ed30f6ef8b5e401e193e49e2410e836209ab
davislyu <davislyu@gmail.com>
2023-09-12 22:25:27 +0300

    Added Media queries

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./navbar.css";
 import { MdOutlineTravelExplore } from "react-icons/md";
 import { AiFillCloseCircle } from "react-icons/ai";
 import { PiDotsNineBold } from "react-icons/pi";
-
+import Aos from "aos";
+import "aos/dist/aos.css";
 const Navbar = () => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
   const [active, setActive] = useState("navBar");
   // fucntion to toggle the Navbar
   const showNav = () => {
@@ -28,7 +32,7 @@ const Navbar = () => {
           </a>
         </div>
         <div className={active}>
-          <ul className="navBarLists flex">
+          <ul data-aos="fade-right" className="navBarLists flex">
             <li className="navItem">
               <a href="#" className="navLink">
                 Home
