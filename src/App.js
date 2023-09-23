14a386536c93769ac1683d4c8cfaa88da383c182
davislyu <davislyu@gmail.com>
2023-09-23 16:55:41 +0300

    Updated imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import "./app.css"
+import "./App.css"
 import Navbar from './components/Navbar/Navbar.jsx'
 import Home from './components/Home/Home.jsx'
 import Footer from './components/Footer/Footer.jsx'
